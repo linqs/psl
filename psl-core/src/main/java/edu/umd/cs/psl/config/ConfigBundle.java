4ff74b769b46fd151f780049cf7b334d3e5ce454
Stephen Bach <bach@cs.umd.edu>
2011-07-31 22:32:45 -0400

    Started config manager and bundle.

---
+++ psl-core/src/main/java/edu/umd/cs/psl/config/ConfigBundle.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the PSL software.
+ * Copyright 2011 University of Maryland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.umd.cs.psl.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigBundle {
+	
+	private String id;
+	
+	private Map<String, Object> settings;
+	
+	private static final Logger log = LoggerFactory.getLogger(ConfigBundle.class);
+
+	public ConfigBundle(String bundleId) {
+		id = bundleId;
+		settings = new HashMap<String, Object>();
+	}
+	
+	public void set(String key, Object value) {
+		
+	}
+}
