585da57ad96036d6778559a1859e5ef29beeff8e
Stephen Bach <bach@cs.umd.edu>
2011-08-18 12:30:40 -0400

    Added more trace output and a couple of minor bug fixes to HomogeneousIPM.

--- psl-core/src/main/java/edu/umd/cs/psl/optimizer/conic/ipm/HomogeneousIPM.java
+++ psl-core/src/main/java/edu/umd/cs/psl/optimizer/conic/ipm/HomogeneousIPM.java
@@ -173,15 +173,12 @@ public class HomogeneousIPM implements ConicProgramSolver {
 		
 		/* Computes values for stopping criteria */
 		double muZero = (x.zDotProduct(s) + vars.tau * vars.kappa) / pm.k;
-		double primalInfRedDenom = Math.max(1.0, alg.norm2(A.zMult(x, null).assign(
-				b.copy().assign(DoubleFunctions.mult(vars.tau))
-				, DoubleFunctions.minus)));
-		double dualInfRedDenom = Math.max(1.0, alg.norm2(A.zMult(x, null).assign(
-				b.copy().assign(DoubleFunctions.mult(vars.tau))
-				, DoubleFunctions.minus)));
-		double gapRedDenom = Math.max(1.0, alg.norm2(A.zMult(x, null).assign(
-				b.copy().assign(DoubleFunctions.mult(vars.tau))
-				, DoubleFunctions.minus)));
+		double primalInfRedDenom = Math.max(1.0, alg.norm2(
+				A.zMult(x, b.copy().assign(DoubleFunctions.mult(vars.tau)), 1.0, -1.0, false)));
+		double dualInfRedDenom = Math.max(1.0, alg.norm2(
+				A.zMult(w, c.copy().assign(DoubleFunctions.mult(vars.tau)), 1.0, -1.0, true)
+				.assign(s, DoubleFunctions.plus)));
+		double gapRedDenom = Math.max(1.0, Math.abs(b.zDotProduct(w) - c.zDotProduct(x) - vars.kappa));
 		
 		stepNum = 0;
 		do {
@@ -203,7 +200,7 @@ public class HomogeneousIPM implements ConicProgramSolver {
 			gap = Math.abs(bDotW - cDotX - vars.kappa) / gapRedDenom;
 			sig = Math.abs(cDotX / vars.tau - bDotW / vars.tau) / (1 + Math.abs(bDotW / vars.tau));
 			
-			log.trace("Itr: {} -- Gap: {} -- P. Inf: {} -- D. Inf: {} -- Sig: {}", new Object[] {++stepNum, mu, primalInfeasibility, dualInfeasibility, sig});
+			log.trace("Itr: {} -- Gap: {} -- Other Gap: {} -- P. Inf: {} -- D. Inf: {} -- Sig: {}", new Object[] {++stepNum, mu, vars.kappa / vars.tau, primalInfeasibility, dualInfeasibility, sig});
 			
 			primalFeasible	= primalInfeasibility <= infeasibilityRedThreshold;
 			dualFeasible	= dualInfeasibility <= infeasibilityRedThreshold;
@@ -452,7 +449,7 @@ public class HomogeneousIPM implements ConicProgramSolver {
 				.assign(DoubleFunctions.mult(gamma-1));
 		res.r2 = A.zMult(w, c.copy().assign(DoubleFunctions.mult(vars.tau)), 1.0, -1.0, true);
 		res.r2.assign(s, DoubleFunctions.plus).assign(DoubleFunctions.mult(gamma-1));
-		res.r3 = (gamma-1) * (b.zDotProduct(w) - c.zDotProduct(x) + vars.kappa);
+		res.r3 = (gamma-1) * (-1*b.zDotProduct(w) + c.zDotProduct(x) - vars.kappa);
 		res.r4 = e.copy().assign(DoubleFunctions.mult(gamma*im.mu)).assign(im.XBar.zMult(im.SBar.zMult(e, null), null), DoubleFunctions.minus);
 		res.r5 = gamma * im.mu - vars.tau * vars.kappa;
 		
@@ -489,8 +486,8 @@ public class HomogeneousIPM implements ConicProgramSolver {
 				, null).assign(DoubleFunctions.mult(-1.0));
 	
 		/* Computes search direction */
-		sd.dTau = (res.r3 + c.zDotProduct(h1) - b.zDotProduct(h2) - res.r5 / vars.tau)
-				/ (-1*(vars.kappa/vars.tau) - c.zDotProduct(im.g1) + b.zDotProduct(im.g2));
+		sd.dTau = (res.r3 - c.zDotProduct(h1) + b.zDotProduct(h2) + res.r5 / vars.tau)
+				/ ((vars.kappa/vars.tau) + c.zDotProduct(im.g1) - b.zDotProduct(im.g2));
 		sd.dx = im.g1.copy().assign(DoubleFunctions.mult(sd.dTau)).assign(h1, DoubleFunctions.plus);
 		sd.dw = im.g2.copy().assign(DoubleFunctions.mult(sd.dTau)).assign(h2, DoubleFunctions.plus);
 		sd.dKappa = (res.r5 - vars.kappa*sd.dTau) / vars.tau;
@@ -499,6 +496,40 @@ public class HomogeneousIPM implements ConicProgramSolver {
 						im.SBar.zMult(pm.T.zMult(im.ThetaW.zMult(sd.dx, null), null), null)
 						, DoubleFunctions.minus)
 				, null), null), null);
+		
+		/* Computes how closely the system was solved */
+		DenseDoubleAlgebra alg = new DenseDoubleAlgebra();
+		log.trace("First equations: {}", alg.norm2(program.getA().zMult(sd.dx, program.getB().copy().assign(DoubleFunctions.mult(sd.dTau)), 1.0, -1.0, false)
+				.assign(res.r1, DoubleFunctions.minus)));
+		log.trace("Second equations: {}", alg.norm2(program.getA().zMult(sd.dx, program.getB().copy().assign(DoubleFunctions.mult(sd.dTau)), 1.0, -1.0, false)
+				.assign(res.r1, DoubleFunctions.minus)));
+		log.trace("Third equations: {}", program.getC().zDotProduct(sd.dx) - program.getB().zDotProduct(sd.dw) - sd.dKappa - res.r3);
+		log.trace("Fourth equations: {}", alg.norm2(im.XBar.zMult(pm.T.zMult(im.invThetaInvW.zMult(sd.ds, null), null), null)
+				.assign(im.SBar.zMult(pm.T.zMult(im.ThetaW.zMult(sd.dx, null), null), null), DoubleFunctions.plus)
+				.assign(res.r4, DoubleFunctions.minus)));
+		log.trace("Fifth equations: {}", vars.tau*sd.dKappa + vars.kappa*sd.dTau - res.r5);
+		
+		/* Computes properties of search direction */
+		log.trace("Norm primal: {}", alg.norm2(A.zMult(program.getX().copy().assign(DoubleFunctions.mult(1-gamma)).assign(sd.dx, DoubleFunctions.plus), null)
+				.assign(b.copy().assign(DoubleFunctions.mult((1-gamma)*vars.tau+sd.dTau)), DoubleFunctions.minus)));
+		log.trace("Norm dual: {}", alg.norm2(A.zMult(program.getW().copy().assign(DoubleFunctions.mult(1-gamma)).assign(sd.dw, DoubleFunctions.plus), null, 1.0, 0.0, true)
+				.assign(program.getS().copy().assign(DoubleFunctions.mult(1-gamma)).assign(sd.ds, DoubleFunctions.plus), DoubleFunctions.plus)
+				.assign(program.getC().copy().assign(DoubleFunctions.mult((1-gamma)*vars.tau+sd.dTau)), DoubleFunctions.minus)));
+		log.trace("Gap condition: {}", c.zDotProduct(program.getX().copy().assign(DoubleFunctions.mult(1-gamma)).assign(sd.dx, DoubleFunctions.plus))
+				- b.zDotProduct(program.getW().copy().assign(DoubleFunctions.mult(1-gamma)).assign(sd.dw, DoubleFunctions.plus))
+				- ((1-gamma) * vars.kappa + sd.dKappa));
+		
+		/* Verifies consequences of properties */
+		log.trace("First consequence: {}", program.getX().copy().assign(DoubleFunctions.mult(1-gamma)).assign(sd.dx, DoubleFunctions.plus)
+				.zDotProduct(program.getS().copy().assign(DoubleFunctions.mult(1-gamma)).assign(sd.ds, DoubleFunctions.plus))
+				+ ((1-gamma) * vars.kappa + sd.dKappa) * ((1-gamma)*vars.tau+sd.dTau));
+		
+		/* Computes another property of the search direction */
+		log.trace("Other condition: {}", program.getX().zDotProduct(sd.ds) + program.getS().zDotProduct(sd.dx) + vars.tau*sd.dKappa + vars.kappa*sd.dTau
+				- (gamma - 1) * im.mu * pm.k);
+		
+		/* Verifies orthogonality of search direction components */
+		log.trace("Step dot: {}", sd.dx.zDotProduct(sd.ds) + sd.dTau * sd.dKappa);
 		
 		return sd;
 	}
