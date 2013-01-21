023a0307686520daff1c46cf4b0dd52ba6966ff4
Stephen Bach <bach@cs.umd.edu>
2013-01-21 17:59:29 -0500

    Added DataStore test.

--- psl-core/src/test/java/edu/umd/cs/psl/database/DataStoreContractTest.java
+++ psl-core/src/test/java/edu/umd/cs/psl/database/DataStoreContractTest.java
@@ -55,6 +55,7 @@ abstract public class DataStoreContractTest {
 	private static StandardPredicate p1;
 	private static StandardPredicate p2;
 	private static StandardPredicate p3;
+	private static StandardPredicate p4;
 	private static FunctionalPredicate fp1;
 	
 	private DataStore datastore;
@@ -78,6 +79,7 @@ abstract public class DataStoreContractTest {
 		p1 = predicateFactory.createStandardPredicate("P1", ArgumentType.UniqueID, ArgumentType.UniqueID);
 		p2 = predicateFactory.createStandardPredicate("P2", ArgumentType.String, ArgumentType.String);
 		p3 = predicateFactory.createStandardPredicate("P3", ArgumentType.Double, ArgumentType.Double);
+		p4 = predicateFactory.createStandardPredicate("P4", ArgumentType.UniqueID, ArgumentType.Double);
 		fp1 = predicateFactory.createFunctionalPredicate("FP1", new ExternalFunction() {
 			
 			@Override
@@ -325,6 +327,92 @@ abstract public class DataStoreContractTest {
 		atom = db.getAtom(fp1, new DoubleAttribute(0.0), new DoubleAttribute(0.0));
 		assertEquals(0.0, atom.getValue(), 0.0);
 		assertTrue(Double.isNaN(atom.getConfidenceValue()));
+	}
+	
+	@Test
+	public void testExecuteQuery() {
+		Inserter inserter;
+		Database db;
+		Formula formula;
+		ResultList results;
+		GroundTerm[] grounding;
+		
+		UniqueID a = datastore.getUniqueID(0);
+		UniqueID b = datastore.getUniqueID(1);
+		UniqueID c = datastore.getUniqueID(2);
+		UniqueID d = datastore.getUniqueID(3);
+		UniqueID e = datastore.getUniqueID(4);
+		UniqueID f = datastore.getUniqueID(5);
+		
+		Variable X = new Variable("X");
+		Variable Y = new Variable("Y");
+		
+		datastore.registerPredicate(p1);
+		inserter = datastore.getInserter(p1, new Partition(0));
+		inserter.insert(a, b);
+		
+		db = datastore.getDatabase(new Partition(0));
+		
+		formula = new QueryAtom(p1, X, Y);
+		results = db.executeQuery(new DatabaseQuery(formula));
+		assertEquals(1, results.size());
+		assertEquals(a, results.get(0, X));
+		assertEquals(b, results.get(0, Y));
+		
+		grounding = results.get(0);
+		assertEquals(a, grounding[0]);
+		assertEquals(b, grounding[1]);
+		
+		db.close();
+		
+		datastore.registerPredicate(p4);
+		inserter = datastore.getInserter(p4, new Partition(0));
+		inserter.insert(a, -0.1);
+		
+		db = datastore.getDatabase(new Partition(0));
+		
+		formula = new QueryAtom(p4, X, Y);
+		results = db.executeQuery(new DatabaseQuery(formula));
+		assertEquals(1, results.size());
+		assertEquals(a, results.get(0, X));
+		assertEquals(new DoubleAttribute(-0.1), results.get(0, Y));
+		
+		grounding = results.get(0);
+		assertEquals(a, grounding[0]);
+		assertEquals(new DoubleAttribute(-0.1), grounding[1]);
+		
+		formula = new QueryAtom(p4, X, Y);
+		results = db.executeQuery(new DatabaseQuery(formula));
+		
+		db.close();
+		
+		inserter.insert(b, 4.0);
+		inserter.insert(c, 4.0);
+		inserter.insert(d, 4.0);
+		inserter.insert(e, 4.0);
+		inserter.insert(f, 4.0);
+		
+		db = datastore.getDatabase(new Partition(0));
+		
+		results = db.executeQuery(new DatabaseQuery(formula));
+		assertEquals(6, results.size());
+		for (int i = 0; i < 6; i++) {
+			assertTrue(results.get(i)[0] instanceof UniqueID);
+		}
+		
+		formula = new Conjunction(new QueryAtom(p4, X, new DoubleAttribute(4.0)),
+						new QueryAtom(p1, Y, X));
+		results = db.executeQuery(new DatabaseQuery(formula));
+		assertEquals(1, results.size());
+		assertEquals(b, results.get(0)[0]);
+		assertEquals(a, results.get(0)[1]);
+		
+		formula = new Conjunction(new QueryAtom(p1, Y, X),
+				new QueryAtom(p4, X, new DoubleAttribute(4.0)));
+		results = db.executeQuery(new DatabaseQuery(formula));
+		assertEquals(1, results.size());
+		assertEquals(a, results.get(0)[0]);
+		assertEquals(b, results.get(0)[1]);
 	}
 	
 	@Test
