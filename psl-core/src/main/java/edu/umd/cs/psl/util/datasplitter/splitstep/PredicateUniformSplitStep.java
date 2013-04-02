7500d63aff24530cb1204eb8ba39045abb6257ac
Bert Huang <bert@cs.umd.edu>
2013-04-02 16:54:43 -0400

    Removed round robin split step and abstract layering; random is almost always preferred. Added query random split step

--- psl-core/src/main/java/edu/umd/cs/psl/util/datasplitter/splitstep/PredicateUniformSplitStep.java
+++ psl-core/src/main/java/edu/umd/cs/psl/util/datasplitter/splitstep/PredicateUniformSplitStep.java
@@ -3,23 +3,100 @@ package edu.umd.cs.psl.util.datasplitter.splitstep;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
+import edu.umd.cs.psl.database.Database;
+import edu.umd.cs.psl.database.Partition;
 import edu.umd.cs.psl.database.loading.Inserter;
+import edu.umd.cs.psl.model.argument.GroundTerm;
 import edu.umd.cs.psl.model.atom.GroundAtom;
 import edu.umd.cs.psl.model.predicate.StandardPredicate;
+import edu.umd.cs.psl.util.database.Queries;
 
-public class PredicateUniformSplitStep extends PredicateSplitStep {
+public class PredicateUniformSplitStep implements SplitStep {
+	private static final int NO_GROUP = -1;
 
-	public PredicateUniformSplitStep(StandardPredicate target, int numFolds,
-			int groupBy) {
-		super(target, numFolds, groupBy);
+	private StandardPredicate target;
+	private int numFolds;
+	private int groupBy;
+
+	/**
+	 * Constructor for splitting by GroundTerms
+	 * @param target Predicate whose groundings to split
+	 * @param numFolds
+	 * @param groupBy index of node argument in groundings
+	 */
+	public PredicateUniformSplitStep(StandardPredicate target, int numFolds, int groupBy) {
+		this.target = target;
+		this.numFolds = numFolds;
+		this.groupBy = groupBy;
+	}
+
+	/**
+	 * Constructor for splitting by GroundAtoms. Does not group atoms, instead treats each 
+	 * atom as its own group
+	 * @param target Predicate whose groundings to split on
+	 * @param numFolds 
+	 */
+	public PredicateUniformSplitStep(StandardPredicate target, int numFolds) {
+		this(target, numFolds, NO_GROUP);
 	}
 
 	@Override
-	protected void insertIntoPartitions(Collection<Set<GroundAtom>> groups, 
+	public List<Collection<Partition>> getSplits(Database inputDB, Random random) {
+		Map<GroundTerm, Set<GroundAtom>> groupMap = new HashMap<GroundTerm, Set<GroundAtom>>();
+		Collection<Set<GroundAtom>> groups;
+		List<Collection<Partition>> splits = new ArrayList<Collection<Partition>>();
+
+		Set<GroundAtom> allAtoms = Queries.getAllAtoms(inputDB, target);
+
+		if (groupBy == NO_GROUP) {
+			groups = new ArrayList<Set<GroundAtom>>(allAtoms.size());
+			for (GroundAtom atom : allAtoms) {
+				Set<GroundAtom> group = new HashSet<GroundAtom>();
+				group.add(atom);
+				groups.add(group);
+			}
+		} else {
+			// group atoms
+			for (GroundAtom atom : allAtoms) {
+				GroundTerm key = atom.getArguments()[groupBy];
+				if (groupMap.get(key) == null) {
+					groupMap.put(key, new TreeSet<GroundAtom>());
+				}
+				groupMap.get(key).add(atom);
+			}
+			groups = groupMap.values();
+		}		
+
+		List<Partition> allPartitions = new ArrayList<Partition>();
+		List<Inserter> inserters = new ArrayList<Inserter>();
+		for (int i = 0; i < numFolds; i++) {
+			Partition nextPartition = inputDB.getDataStore().getNextPartition(); 
+			allPartitions.add(nextPartition);
+			inserters.add(inputDB.getDataStore().getInserter(target, nextPartition));
+		}
+
+		insertIntoPartitions(groups, inserters, random);
+
+		for (int i = 0; i < numFolds; i++) {
+			Set<Partition> partitions = new TreeSet<Partition>();
+			for (int j = 0; j < numFolds; j++) 
+				if (j != i)
+					partitions.add(allPartitions.get(j));
+			splits.add(partitions);
+		}
+
+		return splits;
+	}
+
+	private void insertIntoPartitions(Collection<Set<GroundAtom>> groups, 
 			List<Inserter> inserters, Random random) {
 		
 		ArrayList<Set<GroundAtom>> groupList = new ArrayList<Set<GroundAtom>>(groups.size());
