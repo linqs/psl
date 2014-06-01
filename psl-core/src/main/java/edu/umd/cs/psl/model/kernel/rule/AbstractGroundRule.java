f6112536127f08345a1aef4818b4084a49be0d66
Stephen Bach <bach@cs.umd.edu>
2014-06-01 11:29:12 -0400

    Removed unnecessary clipping in computation of rule truth values.

--- psl-core/src/main/java/edu/umd/cs/psl/model/kernel/rule/AbstractGroundRule.java
+++ psl-core/src/main/java/edu/umd/cs/psl/model/kernel/rule/AbstractGroundRule.java
@@ -110,7 +110,7 @@ abstract public class AbstractGroundRule implements GroundKernel {
 	}
 	
 	public double getTruthValue() {
-		return 1 - Math.max(getFunction().getValue(), 0);
+		return 1 - getFunction().getValue();
 	}
 	
 	@Override
