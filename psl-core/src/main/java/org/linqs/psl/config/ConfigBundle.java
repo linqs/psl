78c8482e5d7a1491f7ce42c34b20798dd1e51456
Eriq Augustine <eriq.augustine@gmail.com>
2018-01-02 14:54:09 -0800

    Updated copyright for 2018.

--- psl-core/src/main/java/org/linqs/psl/config/ConfigBundle.java
+++ psl-core/src/main/java/org/linqs/psl/config/ConfigBundle.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the PSL software.
  * Copyright 2011-2015 University of Maryland
- * Copyright 2013-2017 The Regents of the University of California
+ * Copyright 2013-2018 The Regents of the University of California
  * Copyright 2001-2008 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
