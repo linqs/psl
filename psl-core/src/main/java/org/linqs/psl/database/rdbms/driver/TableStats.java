3018e4068834b1f295d9066a1576d16789937fa5
Eriq Augustine <eriq.augustine@gmail.com>
2018-08-25 22:40:48 -0700

    Working on histogram-based table selectivity.

--- psl-core/src/main/java/org/linqs/psl/database/rdbms/driver/TableStats.java
+++ psl-core/src/main/java/org/linqs/psl/database/rdbms/driver/TableStats.java
@@ -18,6 +18,7 @@
 package org.linqs.psl.database.rdbms.driver;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,19 +27,123 @@ import java.util.Map;
  */
 public class TableStats {
 	private int count;
+
+	/**
+	 * Selectivity by column.
+	 * [0, 1].
+	 */
 	private Map<String, Double> selectivity;
+
+	/**
+	 * The boundaries in a historgram of values for each column.
+	 * Although start/end boundaries are specified,
+	 * any values past those extremes will be moved into the first/last bucket.
+	 */
+	private Map<String, List<Comparable>> histogramBounds;
+
+	/**
+	 * The counts for each histogram bucket.
+	 */
+	private Map<String, List<Integer>> histogramCounts;
 
 	public TableStats(int count) {
 		this.count = count;
 		this.selectivity = new HashMap<String, Double>();
+		this.histogramBounds = new HashMap<String, List<Comparable>>();
+		this.histogramCounts = new HashMap<String, List<Integer>>();
 	}
 
 	public void addColumnSelectivity(String column, double columnSelectivity) {
-		this.selectivity.put(column.toUpperCase(), new Double(columnSelectivity));
+		column = column.toUpperCase();
+		this.selectivity.put(column, new Double(columnSelectivity));
+	}
+
+	public void addColumnHistogram(String column, List<Comparable> bounds, List<Integer> counts) {
+		column = column.toUpperCase();
+		histogramBounds.put(column, bounds);
+		histogramCounts.put(column, counts);
 	}
 
 	public int getCount() {
 		return count;
+	}
+
+	/**
+	 * Get the selectivity of a range of values for a column.
+	 * For this, we will look up the matching bucket(s) in the histogram.
+	 * If either bound is null or past the extremes, assume that it is the extreme.
+	 * Will fallback to normal selectivity if the histogram is not available.
+	 */
+	public double getHistogramSelectivity(String column, Comparable lowerValue, Comparable upperValue) {
+		if (lowerValue.getClass() != upperValue.getClass()) {
+			throw new IllegalArgumentException(String.format(
+					"Lower and upper value types must match. Got %s and %s.",
+					lowerValue.getClass().getName(),
+					upperValue.getClass().getName()));
+		}
+
+		column = column.toUpperCase();
+
+		List<Comparable> bounds = histogramBounds.get(column);
+		List<Integer> counts = histogramCounts.get(column);
+
+		if (bounds == null || bounds.size() == 0) {
+			return getSelectivity(column);
+		}
+
+		if (lowerValue == null || uncheckedCompare(lowerValue, bounds.get(0)) < 0) {
+			lowerValue = bounds.get(0);
+		}
+
+		if (upperValue == null || uncheckedCompare(upperValue, bounds.get(bounds.size() - 1)) > 0) {
+			upperValue = bounds.get(bounds.size() - 1);
+		}
+
+		if (lowerValue instanceof Integer) {
+			return getHistogramSelectivity(column, ((Integer)lowerValue).intValue(), ((Integer)upperValue).intValue());
+		}
+
+		int totalCount = 0;
+		Comparable currentValue = lowerValue;
+
+		for (int bucket = 1; bucket < bounds.size(); bucket++) {
+			// If the current value is in this bucket, than add the count to the total.
+			// Note that we started at index 1 and are only looking at the end.
+			if (uncheckedCompare(currentValue, bounds.get(bucket)) < 0) {
+				totalCount += counts.get(bucket).intValue();
+			}
+		}
+
+		return (double)totalCount / count;
+	}
+
+	private double getHistogramSelectivity(String column, int lowerValue, int upperValue) {
+		column = column.toUpperCase();
+
+		List<Comparable> bounds = histogramBounds.get(column);
+		List<Integer> counts = histogramCounts.get(column);
+
+		int totalCount = 0;
+		int currentValue = lowerValue;
+
+		for (int bucket = 0; bucket < bounds.size() - 1; bucket++) {
+			int bucketStart = ((Integer)bounds.get(bucket)).intValue();
+			int bucketEnd = ((Integer)bounds.get(bucket + 1)).intValue();
+
+			if (currentValue > bucketEnd) {
+				break;
+			}
+
+			// Assume a uniform distribution within the bucket.
+			totalCount += (double)(currentValue - bucketStart) / (bucketEnd - bucket) * counts.get(bucket);
+		}
+
+		return (double)totalCount / count;
+	}
+
+	@SuppressWarnings("unchecked")
+	private int uncheckedCompare(Comparable a, Object b) {
+		return a.compareTo(b);
 	}
 
 	/**
