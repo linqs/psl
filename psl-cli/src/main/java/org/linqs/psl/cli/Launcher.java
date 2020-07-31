2514087275c9d7a045d681da4bd6b4636d310393
Tarun Salh <tsalh2019@gmail.com>
2020-07-31 04:32:03 -0700

    Remove unused argument.

--- psl-cli/src/main/java/org/linqs/psl/cli/Launcher.java
+++ psl-cli/src/main/java/org/linqs/psl/cli/Launcher.java
@@ -219,7 +219,7 @@ public class Launcher {
         }
 
         if (parsedOptions.hasOption(CommandLineLoader.OPTION_VISUALIZATION)) {
-            visualization(model, inferenceApplication, dataStore, database, closedPredicates);
+            visualization(inferenceApplication, dataStore, database, closedPredicates);
         }
 
         log.info("Inference Complete");
@@ -230,7 +230,7 @@ public class Launcher {
         return database;
     }
 
-    private void visualization(Model model, InferenceApplication inferenceApplication, DataStore dataStore,
+    private void visualization(InferenceApplication inferenceApplication, DataStore dataStore,
             Database predictionDatabase, Set<StandardPredicate> closedPredicates) {
         Set<StandardPredicate> openPredicates = dataStore.getRegisteredPredicates();
         openPredicates.removeAll(closedPredicates);
