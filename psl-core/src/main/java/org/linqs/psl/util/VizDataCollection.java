1660ce7f96b74c1eaf41ca64171b4b246b2dfc0f
Aaron Rodden <aaron.rodden21@gmail.com>
2020-08-21 15:16:34 -0700

    Added functionallity to create a Non-DNF ground rule, which will then go into the newly designed data file

--- psl-core/src/main/java/org/linqs/psl/util/VizDataCollection.java
+++ psl-core/src/main/java/org/linqs/psl/util/VizDataCollection.java
@@ -2,6 +2,7 @@ package org.linqs.psl.util;
 
 import org.linqs.psl.grounding.GroundRuleStore;
 import org.linqs.psl.model.atom.Atom;
+import org.linqs.psl.model.rule.AbstractRule;
 import org.linqs.psl.model.atom.GroundAtom;
 import org.linqs.psl.model.rule.GroundRule;
 import org.linqs.psl.model.rule.Rule;
@@ -17,10 +18,13 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.GZIPOutputStream;
 
 public class VizDataCollection {
@@ -112,14 +116,99 @@ public class VizDataCollection {
             }
         }
     }
+
+    public static List<String> matchAll(Pattern p, String targetString) {
+        List<String> matches = new ArrayList<String>();
+        Matcher m = p.matcher(targetString);
+        while (m.find()) {
+          matches.add(m.group());
+          System.out.println(m.group());
+          System.out.println(m.start());
+          System.out.println(m.end());
+        }
+        return matches;
+    }
+
+    public static String createLogicalGroundRule(AbstractLogicalRule parentRule, Map<String, String> varConstMap) {
+        // Create patterns to find predicates / constants and label to be placed on them.
+        Pattern predicatePattern = Pattern.compile("\\w+\\s*\\(");
+        Pattern constantPattern = Pattern.compile("\\'\\w+\\'");
+        String nonVariableLabel = "__0_";
+
+        // Find all instances of predicates and constants in the parent rule.
+        Matcher predicateMatcher = predicatePattern.matcher(parentRule.getName());
+        Matcher constantMatcher = constantPattern.matcher(parentRule.getName());
+
+        // Collect indices for all predicates and constants so we can label them.
+        ArrayList<Integer> indicies = new ArrayList<Integer>();
+        while (predicateMatcher.find()) {
+            indicies.add(predicateMatcher.start());
+        }
+        while (constantMatcher.find()) {
+            indicies.add(constantMatcher.start());
+        }
+
+        //Sort in descending order so we can places labels with collected indicies.
+        Collections.sort(indicies, Collections.reverseOrder());
+
+        // Apply the lables to a copy of the parent rule.
+        String createdGroundRule = parentRule.getName();
+        for (int index : indicies){
+            createdGroundRule = createdGroundRule.substring(0,index) + nonVariableLabel + createdGroundRule.substring(index);
+        }
+
+        // Replace all variables in the labeled parent rule.
+        for (Map.Entry<String, String> entry : varConstMap.entrySet()) {
+            String re = "\\b"+entry.getKey()+"\\b";
+            // Add surrounding single quotes to variables;
+            String constant = "\'" + entry.getValue() + "\'";
+            createdGroundRule = createdGroundRule.replaceAll(re, constant);
+        }
+
+        // Get rid of all labels.
+        createdGroundRule = createdGroundRule.replaceAll(nonVariableLabel, "");
+
+        return createdGroundRule;
+    }
+
     // TODO: Arithmetic Ground Rules Collection
     public static synchronized void addGroundRule(AbstractLogicalRule parentRule,
             GroundRule groundRule, Map<Variable, Integer> variableMap,  Constant[] constantsList) {
+
+        //TEST NOTE
+        // Arithmetic ground rules will be in non-DNF
+        // normal ground rules will be in dnf
+
+        //TODO:
+        // So maybe when we pass into this function we can have a flag saying if its an
+        // arithmetic or not (or just check the type duh).
+        // If not, we parse how we normally have been doing it (rely on constant map)
+        // If so, then we just parse over non-DNF ground rule
+
+        //This would mean parseing on the parent / ground rule and trying to
+        // recreate as that is just a system for doing this without constant map
+
+        //This should solve parsing problems
+
+        //TODO:
+        // We will also have to think of a way to keep the ID system intact
+        // as our interaction systems rely on those
+
         if (groundRule == null) {
             return;
         }
 
-        // Adds a groundAtom element to Ground Atom Map.
+        // Create the variable constant map used for replacement.
+        Map<String, String> varConstMap = new HashMap<String, String>();
+        for (Map.Entry<Variable, Integer> entry : variableMap.entrySet()) {
+            varConstMap.put(entry.getKey().toString(), constantsList[entry.getValue()].rawToString());
+        }
+
+        createLogicalGroundRule(parentRule, varConstMap);
+
+        System.out.println("<------------------->");
+
+        // Adds a groundAtom element to RuleMap
         ArrayList<Integer> atomHashList = new ArrayList<Integer>();
         HashSet<GroundAtom> atomSet = new HashSet<GroundAtom>(groundRule.getAtoms());
         int atomCount = 0;
@@ -138,12 +227,6 @@ public class VizDataCollection {
         rulesElementItem.put("text", parentRule.getName());
         rulesElementItem.put("weighted", parentRule.isWeighted());
         vizData.rules.put(ruleStringID, rulesElementItem);
-
-        // Adds a groundRule element to Ground Rule Map.
-        Map<String, String> varConstMap = new HashMap<String, String>();
-        for (Map.Entry<Variable, Integer> entry : variableMap.entrySet()) {
-            varConstMap.put(entry.getKey().toString(), constantsList[entry.getValue()].rawToString());
-        }
 
         Map<String, Object> groundRulesElement = new HashMap<String, Object>();
         groundRulesElement.put("ruleID", Integer.parseInt(ruleStringID));
