4265741bd0dfa095e8ac16eff47f1ba14f648902
Charles Dickens <charlieadickens@gmail.com>
2021-07-06 15:06:36 -0700

    Introduce online responses and model information messages. (#309)
    Introduce online responses and model information messages.
    Action status messages are sent to the client after the online psl server executes an online action.
    QueryAtomResponses are sent to the client when it sends a QueryAtom action.
    ModelInformation is sent when the client initially establishes a connection to the server.

---
+++ psl-core/src/main/java/org/linqs/psl/application/inference/online/messages/responses/OnlineResponse.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the PSL software.
+ * Copyright 2011-2015 University of Maryland
+ * Copyright 2013-2021 The Regents of the University of California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.linqs.psl.application.inference.online.messages.responses;
+
+import org.linqs.psl.application.inference.online.messages.OnlineMessage;
+
+import java.util.UUID;
+
+public abstract class OnlineResponse extends OnlineMessage {
+    protected UUID onlineActionID;
+
+    public OnlineResponse(UUID onlineActionID) {
+        super();
+        this.onlineActionID = onlineActionID;
+    }
+
+    public UUID getActionIdentifier() {
+        return onlineActionID;
+    }
+}
