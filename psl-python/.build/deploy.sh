5e911badb04413e41e1c87fd2c3f2b091e6d68f6
Eriq Augustine <eriq.augustine@gmail.com>
2021-09-09 13:42:00 -0700

    Updating the deployment for psl-python to work better with our new CI.

--- psl-python/.build/deploy.sh
+++ psl-python/.build/deploy.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-    echo "USAGE: ${0} [test | release]"
-    exit 1
-fi
+readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly PSLPYTHON_DIR="${THIS_DIR}/.."
 
-repo=''
-if [[ $1 == 'test' ]]; then
-    repo='https://test.pypi.org/legacy/'
-elif [[ $1 == 'release' ]]; then
-    repo='https://upload.pypi.org/legacy/'
-else
-    echo "Unknown deploy type ('${1}'), expected 'test' or 'release'."
-    exit 2
-fi
+function main() {
+    if [[ $# -ne 1 ]]; then
+        echo "USAGE: ${0} [test | release]"
+        exit 1
+    fi
 
-if [[ -z "$TWINE_USERNAME" ]]; then
-    echo "Could not locate twine username (env variable: TWINE_USERNAME)."
-    exit 3
-fi
+    set -e
+    trap exit SIGINT
 
-if [[ -z "$TWINE_PASSWORD" ]]; then
-    echo "Could not locate twine password (env variable: TWINE_PASSWORD)."
-    exit 4
-fi
+    cd "${PSLPYTHON_DIR}"
 
-twine upload --repository-url "${repo}" dist/pslpython-*.whl
+    local repo=''
+    if [[ $1 == 'test' ]]; then
+        repo='https://test.pypi.org/legacy/'
+    elif [[ $1 == 'release' ]]; then
+        repo='https://upload.pypi.org/legacy/'
+    else
+        echo "Unknown deploy type ('${1}'), expected 'test' or 'release'."
+        exit 2
+    fi
+
+    if [[ -z "$TWINE_USERNAME" ]]; then
+        echo "Could not locate twine username (env variable: TWINE_USERNAME)."
+        exit 3
+    fi
+
+    if [[ -z "$TWINE_PASSWORD" ]]; then
+        echo "Could not locate twine password (env variable: TWINE_PASSWORD)."
+        exit 4
+    fi
+
+    python3 -m twine upload --repository-url "${repo}" dist/pslpython-*.whl
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
--- psl-python/.build/package.sh
+++ psl-python/.build/package.sh
@@ -3,5 +3,22 @@
 # Create a Python binary wheel.
 # This will clear out any previous builds.
 
-rm -Rf dist
-python3 setup.py bdist_wheel
+readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly PSLPYTHON_DIR="${THIS_DIR}/.."
+
+function main() {
+    if [[ $# -ne 0 ]]; then
+        echo "USAGE: $0"
+        exit 1
+    fi
+
+    set -e
+    trap exit SIGINT
+
+    cd "${PSLPYTHON_DIR}"
+
+    rm -Rf dist
+    python3 setup.py bdist_wheel
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
