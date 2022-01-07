159afe1599b46b8d8ed57243f3e506bf410e74bb
Eriq Augustine <eriq.augustine@gmail.com>
2022-01-06 19:51:04 -0800

    Updated copyright year to 2022.

--- psl-python/pslpython/cli/__main__.py
+++ psl-python/pslpython/cli/__main__.py
@@ -1,7 +1,7 @@
 """
 This file is part of the PSL software.
 Copyright 2011-2015 University of Maryland
-Copyright 2013-2021 The Regents of the University of California
+Copyright 2013-2022 The Regents of the University of California
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
