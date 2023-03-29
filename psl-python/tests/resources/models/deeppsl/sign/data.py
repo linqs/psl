f93084850f73d24851039a523d7fa029289127e0
Connor Pryor <cfpryor@ucsc.edu>
2023-03-29 10:11:53 -0700

    Fixed sign data generator and added get neural data.

--- psl-python/tests/resources/models/deeppsl/sign/data.py
+++ psl-python/tests/resources/models/deeppsl/sign/data.py
@@ -17,17 +17,23 @@ limitations under the License.
 '''
 
 '''
-This file handles data management for the sign example. When getting data, 
-first attempts to retrieve existing data. If no data is found, then
+This file handles data management for the sign example.
+
+When getting data, first attempts to retrieve existing data. If no data is found, then
 generate and save new data.
 '''
 
+import importlib
 import os
 import random
+import sys
 
-import tests.resources.models.deeppsl.util as util
+THIS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.join(THIS_DIR, '..'))
 
-ENTITY_FEATURE_MAP_FILENAME = 'entity-feature-map.txt'
+util = importlib.import_module("util")
+
+ENTITY_DATA_MAP_FILENAME = 'entity-data-map.txt'
 
 TARGETS_TRAIN_FILENAME = 'targets_train.txt'
 TARGETS_TEST_FILENAME = 'targets_test.txt'
@@ -43,25 +49,33 @@ CLASS_SIZE = 2
 
 SEED = 4
 
-'''
-Get data attempts to load sign data from a specified directory. If the
-data is not found, then generate and save new sign data. This ensures
-that sign data is available for subsequent use.
-'''
-def get_data(out_dir):
+
+def get_psl_data(out_dir):
     if not os.path.isfile(os.path.join(out_dir, CONFIG_FILENAME)):
         x_train, y_train, x_test, y_test = _generate_data()
-        _write_psl_data(out_dir, x_train, y_train, x_test, y_test)
-    else:
-        x_train, y_train, x_test, y_test = _load_psl_data()
+        _write_data(out_dir, x_train, y_train, x_test, y_test)
 
-    return x_train, y_train, x_test, y_test
+    return _load_data(out_dir)
 
-'''
-A simple sign dataset for classifying positive and negative entities. A label
-of [1, 0] means all values will be positive, [0, 1] is all negative.
-'''
+
+def get_neural_data(out_dir):
+    entity_data_map, x_train, y_train, x_test, y_test = get_psl_data(out_dir)
+
+    entity_data_dict = {example[0]: example[0:] for example in entity_data_map}
+
+    neural_x_train = [[int(feature) for feature in entity_data_dict[example[0]][:-1]] for example in x_train]
+    neural_y_train = [int(entity_data_dict[example[0]][-1:][0]) for example in x_train]
+    neural_x_test = [[int(feature) for feature in entity_data_dict[example[0]][:-1]] for example in x_test]
+    neural_y_test = [int(entity_data_dict[example[0]][-1:][0]) for example in x_test]
+
+    return neural_x_train, neural_y_train, neural_x_test, neural_y_test
+
+
 def _generate_data():
+    '''
+    A simple sign dataset for classifying positive and negative entities. A label
+    of [1, 0] means all values will be positive, [0, 1] is all negative.
+    '''
     random.seed(SEED)
 
     x_train = []
@@ -84,12 +98,15 @@ def _generate_data():
 
     return x_train, y_train, x_test, y_test
 
-def _write_psl_data(out_dir, x_train, y_train, x_test, y_test):
+
+def _write_data(out_dir, x_train, y_train, x_test, y_test):
     os.makedirs(out_dir, exist_ok=True)
 
-    features = x_train + x_test
-    features = [[index] + features[index] for index in range(len(features))]
-    util.write_psl_file(os.path.join(out_dir, ENTITY_FEATURE_MAP_FILENAME), features)
+    x_data = x_train + x_test
+    y_data = y_train + y_test
+
+    entity_data_map = [[index] + x_data[index] + [y_data[index][1]] for index in range(len(x_data))]
+    util.write_psl_file(os.path.join(out_dir, ENTITY_DATA_MAP_FILENAME), entity_data_map)
 
     targets_train = [[index] for index in range(len(x_train))]
     util.write_psl_file(os.path.join(out_dir, TARGETS_TRAIN_FILENAME), targets_train)
@@ -112,11 +129,14 @@ def _write_psl_data(out_dir, x_train, y_train, x_test, y_test):
     }
     util.write_json(config, os.path.join(out_dir, CONFIG_FILENAME))
 
-def _load_psl_data(load_path):
+
+def _load_data(load_path):
+    entity_data_map = util.load_psl_file(os.path.join(load_path, ENTITY_DATA_MAP_FILENAME))
+
     x_train = util.load_psl_file(os.path.join(load_path, TARGETS_TRAIN_FILENAME))
     y_train = util.load_psl_file(os.path.join(load_path, TRUTH_TRAIN_FILENAME))
 
     x_test = util.load_psl_file(os.path.join(load_path, TARGETS_TEST_FILENAME))
     y_test = util.load_psl_file(os.path.join(load_path, TRUTH_TEST_FILENAME))
 
-    return x_train, y_train, x_test, y_test
+    return entity_data_map, x_train, y_train, x_test, y_test
